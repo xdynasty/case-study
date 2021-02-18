cf9805253f55f6675a0258895118e73d1b016d28
xdynasty <xdynasty5201314@gmail.com>
2021-02-18 17:38:33 -0500

    complete basic index page

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+$(() => {
+  let flashMsgs = [
+    'Import Fees Included',
+    'Free Express Shipping Over $500',
+    'CNY Sale',
+  ];
+  let flashMsgI = 0;
+  let flashEle = $('#flash');
+  flashEle.text(flashMsgs[0]);
+
+  function changeCurrFlashMsg() {
+    flashMsgI = (flashMsgI + 1) % flashMsgs.length;
+    flashEle.text(flashMsgs[flashMsgI]);
+  }
+
+  setInterval(changeCurrFlashMsg, 2000);
+});
