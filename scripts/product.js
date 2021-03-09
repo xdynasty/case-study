7136671791bc8b0032fe12bf91b4e4787adbde5b
xdynasty <xdynasty5201314@gmail.com>
2021-03-09 10:57:00 -0500

    add interactivity to product page

--- scripts/product.js
+++ scripts/product.js
@@ -1,23 +1,89 @@
 $(() => {
-  let images = [
-    'https://th.bing.com/th/id/OIP.Jf0NnGpH2AhNM3BtwZufwwHaJ4?w=117&h=180&c=7&o=5&dpr=2&pid=1.7',
-    'https://th.bing.com/th/id/OIP.YVhCc6EJPGY-rieLuOXmegHaLG?pid=ImgDet&rs=1',
-    'https://i.pinimg.com/originals/50/b8/4c/50b84c2c87f6bc767bd0fdac2e8fda26.png',
-    'https://th.bing.com/th/id/OIP.jolYGxsmIwxGCtwX24-fYwHaE7',
-  ];
+  let product = {
+    productName: 'LOGO OVER COTTON & WOOL KNIT SWEATER',
+    id: 1,
+    category: 'CLOTHING/KNITWEAR',
+    details: [
+      'Embroidered front logo',
+      'Model is wearing a size M',
+      'Model height: 1.88m',
+      'Cut for an oversize fit',
+      'Buy your usual size',
+      'Composition: 71% Cotton, 12% Wool, 17% Polyamide',
+    ],
+    styleName: 'NAVY',
+    relatedStyles: [
+      { id: 2, styleName: 'BLACK' },
+      { id: 3, styleName: 'GREEN' },
+    ],
+    images: [
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/1.jpeg',
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/2.jpeg',
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/3.jpeg',
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/4.webp',
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/5.jpeg',
+      'https://case-study.s3.us-east-2.amazonaws.com/images/1/6.jpeg',
+    ],
+    sizes: [
+      { size: 'S', quantity: 3 },
+      { size: 'M', quantity: 1 },
+      { size: 'L', quantity: 2 },
+    ],
+  };
+
+  $('#styleSelection').text(product.styleName);
+
+  product.images.forEach((image) => {
+    $('<img>').attr('src', image).appendTo($('#carousel'));
+  });
+
+  if (product.relatedStyles.length) {
+    let styleOptions = $('<div class="style-options options"></div>').appendTo(
+      '#style'
+    );
+    product.relatedStyles.forEach((style) => {
+      $(
+        `<a href="../html/product.html?productId=${style.id}"><div class="style-option">${style.styleName}</div></a>`
+      ).appendTo(styleOptions);
+    });
+    $('#styleSelection').click(() => {
+      if (!$('.style-options').is(':visible')) $('.style-options').show();
+      else $('.style-options').hide();
+    });
+  }
+
+  if (product.sizes.length) {
+    let sizeOptions = $('<div class="size-options options"></div>').appendTo(
+      '#size'
+    );
+    product.sizes.forEach((size) => {
+      $(`<div class="size-option" data-value="${size.size}">${size.size}</div>`)
+        .click(() => {
+          console.log(size);
+          $('#sizeSelection').text(size.size);
+          $('.size-options').hide();
+        })
+        .appendTo(sizeOptions);
+    });
+    $('#sizeSelection').click(() => {
+      if (!$('.size-options').is(':visible')) $('.size-options').show();
+      else $('.size-options').hide();
+    });
+  }
+
+  $('#mainImg').attr('src', product.images[0]);
 
   $('html').click((e) => {
-    // console.log('visible:', $('.color-options').is(':visible'));
+    // console.log('visible:', $('.style-options').is(':visible'));
     if (
-      !$(e.target).closest('.color-options').length &&
-      !$(e.target).closest('#colorSelection').length &&
-      $('.color-options').is(':visible')
+      !$(e.target).closest('#styleSelection').length &&
+      $('.style-options').is(':visible')
     ) {
-      $('.color-options').hide();
+      $('.style-options').hide();
     }
     if (
-      !$(e.target).closest('.size-options').length &&
       !$(e.target).closest('#sizeSelection').length &&
+      !$(e.target).closest('.size-option').length &&
       $('.size-options').is(':visible')
     ) {
       $('.size-options').hide();
@@ -25,27 +91,6 @@ $(() => {
   });
 
   $('#carousel img').click(function () {
-    console.log('clicked');
-    console.log($(this).attr('src'));
     $('#mainImg').attr('src', $(this).attr('src'));
-  });
-
-  $('#colorSelection').click(() => {
-    if (!$('.color-options').is(':visible')) $('.color-options').show();
-    else $('.color-options').hide();
-  });
-  $('#sizeSelection').click(() => {
-    if (!$('.size-options').is(':visible')) $('.size-options').show();
-    else $('.size-options').hide();
-  });
-
-  $('.color-option').click(function () {
-    $('.color-options').hide();
-    $('#colorSelection').text($(this).data('value'));
-  });
-
-  $('.size-option').click(function () {
-    $('.size-options').hide();
-    $('#sizeSelection').text($(this).data('value'));
   });
 });
